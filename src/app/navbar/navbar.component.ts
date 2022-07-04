ca8e33eeabc8d5d17d0eca6067ad5169de441338
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-07-04 16:58:08 -0500

    add navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  
+  menuOpen = false
+  toggleMenu = stuf => {
+    this.menuOpen = stuf
+  }
+  pages = [
+    {
+      url: "/scribble",
+      title: "scribble",
+      desc: "ruins your drawings"
+    },
+    {
+      url: "//car-crash.herokuapp.com",
+      title: "car crash",
+      desc: "multiplayer car game"
+    },
+    {
+      url: "/thesaurus-rex",
+      title: "thesaurus rex",
+      desc: "dinosaur that knows english"
+    }
+  ]
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
