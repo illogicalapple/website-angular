e319cef3cd10679b3e035b26df5eaf569f62f2b9
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-07-05 10:11:31 -0500

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar/navbar.component.html',
+  styleUrls: ['./navbar/navbar.component.css']
+})
+
 export class AppComponent {
   title = 'my-app';
 }
