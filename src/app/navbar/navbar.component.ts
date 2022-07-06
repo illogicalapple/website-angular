6038422d39eb743a1e5ab5e19cc374ddc0db7403
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-07-06 10:31:54 -0500

    Update navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   
   menuOpen = false
-  toggleMenu = (stuf: Boolean) => {
+  toggleMenu = (stuf: boolean) => {
     this.menuOpen = stuf
   }
   pages = [
